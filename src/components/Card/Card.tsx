7f53ab7758b3b7225fcac470e62288d84affb6ff
DioJun <junior_dionizio@outlook.com.br>
2024-02-08 20:29:27 -0300

    Nova Atualização

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,10 +2,38 @@ import './Card.css'
 
 export const Card = () => {
     return(
-        <div className="conatiner">
+        <div className="conatiner-card">
 
             <div className='slider'>
-                <h1>Profissionais qualificados em instalação e manutenção de ar condicionados e geladeiras</h1>
+
+                <h1> A One Refrigeração tem a Solução que você precisa</h1>
+                <h2>Conheça nossos serviços e benefícios: </h2>
+
+                <div className='services'>
+
+                    <span className='card card1'>
+                        <div>
+                            <img src='./cardImg/1.jpg' alt='01' />
+                            <p>Instalação e Soluções</p>
+                        </div>
+                    </span>
+
+                    <span className='card card2'>
+                        <div>
+                            <img src="/images/icon-box-lightning.svg" alt="" />
+                            <p>Conserto e Manutenção</p>
+                        </div>
+                    </span>
+
+                    <span className='card card3'>
+                        <div>
+                            <img src="/images/icon-box-lightning.svg" alt="" />
+                            <p>Qualidade e Confiança</p>
+                        </div>
+                    </span>
+                
+                </div>
+
             </div>
 
         </div>

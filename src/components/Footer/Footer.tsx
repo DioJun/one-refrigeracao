0fea5bfa669838bf045186ae46afb265c6b29a1a
DioJun <junior_dionizio@outlook.com.br>
2024-02-19 16:07:01 -0300

    aatualização se footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,12 @@
-import { AbsoluteCenter, Box, Center} from "@chakra-ui/react";
-import { FaFacebook} from "react-icons/fa";
-import { RiInstagramFill } from "react-icons/ri";
+// import { AbsoluteCenter, Box, Center} from "@chakra-ui/react";
+// import { FaFacebook} from "react-icons/fa";
+// import { RiInstagramFill } from "react-icons/ri";
 import './Footer.css'
 
 export const Footer = () => {
     return(
         <div className="footerContainer">
-            <Center>
+            {/* <Center>
                 <Box width='700px' height='300px' position='relative' padding='10'>
                     
                     <AbsoluteCenter display={'flex'}>
@@ -15,7 +15,7 @@ export const Footer = () => {
                     </AbsoluteCenter>
 
                 </Box>
-            </Center>
+            </Center> */}
 
             <a className="developed" href="https://www.workana.com/freelancer/1c944ab88433a73d9ef59a489eb16ffc" target="blank">Desenvolvido por C.A.Y.F</a>
 
